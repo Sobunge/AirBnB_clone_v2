fc3e191ad28b5dc96802f0f04c349a479cb0fdf4
Samuel Obunge <samuelobunge@yahoo.com>
2024-03-20 17:04:35 +0300

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -17,7 +17,7 @@ class test_City(test_basemodel):
 
     def test_state_id(self):
         """ """
-        new = self.value()
+        new = self.value(state_id="test")
         self.assertEqual(type(new.state_id), str)
 
     def test_name(self):
