7d30f0ff4d733a894fc87194b77f225024a0a5fd
Samuel Obunge <samuelobunge@yahoo.com>
2024-03-20 17:13:29 +0300

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -2,6 +2,7 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
+from models.base_model import BaseModel
 import pycodestyle
 import unittest
 import pep8
