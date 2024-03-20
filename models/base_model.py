15f551e31eed6567a88eca6a137d92652fa68cd2
samuel obunge <samuelobunge@gmail.com>
2024-03-20 03:56:15 -0700

    changes

--- models/base_model.py
+++ models/base_model.py
@@ -36,13 +36,8 @@ class BaseModel:
                     setattr(self, key, value)
             if "id" not in kwargs:
                 self.id = str(uuid.uuid4())
-            if "created_at" not in kwargs:
-                self.created_at = datetime.now()
-            if "updated_at" not in kwargs:
-                self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """returns a string
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -15,20 +15,20 @@ class test_User(test_basemodel):
 
     def test_first_name(self):
         """ """
-        new = self.value()
+        new = self.value(email="test@example.com", password="test")
         self.assertTrue(isinstance(new.first_name, str) or new.first_name is None)
 
     def test_last_name(self):
         """ """
-        new = self.value()
+        new = self.value(email="test@example.com", password="test")
         self.assertTrue(isinstance(new.last_name, str) or new.last_name is None)
 
     def test_email(self):
         """ """
-        new = self.value()
+        new = self.value(email="test@example.com", password="test")
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
         """ """
-        new = self.value()
+        new = self.value(email="test@example.com", password="test")
         self.assertEqual(type(new.password), str)
