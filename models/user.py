2ee0af106074d0d885b65e4631a4462c8ee6217b
Samuel Obunge <samuelobunge@yahoo.com>
2024-03-21 02:34:23 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,36 +1,13 @@
 #!/usr/bin/python3
-"""This is the user class"""
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+"""User module"""
 from models.base_model import BaseModel, Base
-from models.place import Place
-from models.review import Review
-
+from sqlalchemy import Column, String
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = "users"
+    """User class"""
+    __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
-
-    def __init__(self, email, password, first_name=None, last_name=None):
-        """Initializes a new User instance."""
-        if not isinstance(password, str):
-            raise TypeError("Password must be a string")
-        super().__init__(email=email, password=password,
-                         first_name=first_name,
-                         last_name=last_name)
